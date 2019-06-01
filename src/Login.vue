f66e3ce02adad0875d11b1fb91eb3e7447432370
AdamKoliweszka <adamkoliweszka@gmail.com>
2019-06-01 17:26:47 +0200

    style login form

--- src/Login.vue
+++ src/Login.vue
@@ -1,20 +1,14 @@
 <template>
   <div>
     <Menu/>
-    <div class="Content">
-      <div>
-        Login:
-        <input type:text v-model="user.username">
-      </div>
-
-      <div>
-        Hasło:
-        <input type="password" v-model="user.password">
-      </div>
-      <div>
+    <form>
+        <label for="username">Login:</label>
+        <input id="username" type="text" v-model="user.username">
+        <label for="nickname">Hasło:</label>
+        <input id="nickname" type="password" v-model="user.password">
         <input type="button" @click="login" value="Zaloguj"/>
-      </div>
-    </div>
+      
+    </form>
   </div>
 </template>
 
@@ -48,8 +42,36 @@ export default {
 
 
 <style scoped>
-.Content {
-  padding: 5px;
+form{
+  border: solid 2px darkblue;
+  box-shadow: 0px 0px 10px darkblue;
+  background-color: rgba(0,0,0,0.7);
+  color: blue;
+  width: 30%;
+  margin-left:auto; 
+  margin-right:auto;
+  margin-top: 10px;
+  margin-bottom: 10px;
+  padding: 10px;
+}
+input[type="text"],input[type="password"]{
+  border: none;
+  border-bottom: 1px solid darkblue;
+  background: transparent;
+  width: 100%;
+}
+input[type="button"]
+{
+  margin-top: 10px;
+  display: block;
+  width: 70%;
+  margin-left: auto;
+  margin-right: auto;
+  background-color: rgba(0,0,255,0.7);
+  border: none;
+}
+*{
+  outline: none;
 }
 </style>
 
