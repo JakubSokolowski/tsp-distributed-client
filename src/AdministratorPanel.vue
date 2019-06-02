68ceca8deaf4ce6791738625591328ee97e49e4f
AdamKoliweszka <adamkoliweszka@gmail.com>
2019-06-02 14:15:05 +0200

    improve administrator panel

--- src/AdministratorPanel.vue
+++ src/AdministratorPanel.vue
@@ -1,45 +1,69 @@
 <template>
   <div>
     <Menu/>
-    <div class="container">
+
+    <div v-for="Problem in problems" class="container" :key="'Problem' + Problem.id">
         <div class="sideBar">
             <input type="button" class="elementLeftBar upArrow" value="▶️">
             <input type="button" class="elementLeftBar downArrow" value="▶️">
         </div>
-        <div class="mainContent"></div>
-        <div class="sideBar">
-            <input type="button" class="element redColor" value="X">
-            <input type="button" class="element greenColor" value="▶️">
-            <input type="button" class="element redColor" value="||">
+        <div class="mainContent">
+            <p>Zlecający: {{Problem.user.username}} Data zlecenia: {{Problem.dateOfOrdering}}</p>
+            <p>Algorytm: {{Problem.algorithm}} Progres: {{Problem.percentageOfProgress}}%</p>
+            <p>Ilość miast: {{Problem.graph.numOfCities}}</p>
+            
         </div>
-        <!--
-      <div class="container2">
-        <input type="button" class="element fontSize redColor" value="X">
-        <input type="button" class="element fontSize greenColor" value="▶️">
-        <input type="button" class="element fontSize redColor" value="||">
-      </div>
-      -->
+        <div v-if="Problem.percentageOfProgress < 100" class="sideBar">
+            <input type="button" v-if="!Problem.solving" class="element greenColor" value="▶️">
+            <input type="button" v-if="Problem.solving" class="element greenColor" value="||">
+            <input type="button" class="element redColor" value="X">
+        </div>
     </div>
   </div>
 </template>
 
 <script>
 import Menu from "@/components/Menu.vue";
+import DataAccess from "@/components/DataAccess.js";
 import { error } from "util";
 export default {
   name: "AdministratorPanel",
   components: {
     Menu
   },
-  data: function() {
-    return {};
+  mounted() {
+    this.getData();
+    //Period 1s
+    this.timer = setInterval(this.getData, 1000);
   },
-  methods: {}
+  data: function() {
+    return {
+      problems: [],
+      comunicats: [],
+      timer: "",
+      prog: 10,
+    };
+  },
+  methods: {
+    getData() {
+      DataAccess.getProblems().then(response => {
+        this.problems = response.data;
+        console.log(this.problems);
+      });
+      this.prog += 1;
+    }
+  },
+  beforeDestroy() {
+    clearInterval(this.timer);
+  }
 };
 </script>
 
 
 <style scoped>
+span{
+    width:100%;
+}
 .upArrow{
     color:green;
     transform: rotate(-90deg);
@@ -64,6 +88,7 @@ export default {
 }
 .mainContent{
     width: 60%;
+    color: blue;
 }
 .container{
   margin-left: auto;
@@ -78,10 +103,10 @@ export default {
 }
 .element{
     background: transparent;
-    line-height: 25%;
+    line-height: 45%;
     font-size: 40px;
     display: block;
-    height: 30%;
+    height: 50%;
     border: none;
 }
 .redColor {
